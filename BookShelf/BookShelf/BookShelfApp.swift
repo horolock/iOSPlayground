7d687d3bed2d061702dd6491ac039a5da4a0a8d2
horolock <horolock96@gmail.com>
2025-11-11 22:16:33 +0900

    Project init

---
+++ BookShelf/BookShelf/BookShelfApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookShelfApp.swift
+//  BookShelf
+//
+//  Created by HoJoonKim on 11/11/25.
+//
+
+import SwiftUI
+
+@main
+struct BookShelfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
